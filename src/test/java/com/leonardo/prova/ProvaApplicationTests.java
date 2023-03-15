4a9fc00b625f95b5c3da8b3468bf9a47ddcb46e7
Leonardo Carlotto <958772@lojasrenner.com.br>
2023-03-15 17:52:27 -0300

    create unit test

--- src/test/java/com/leonardo/prova/ProvaApplicationTests.java
+++ src/test/java/com/leonardo/prova/ProvaApplicationTests.java
@@ -1,24 +1,37 @@
 package com.leonardo.prova;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.leonardo.prova.response.ViaCepResponse;
 import com.leonardo.prova.services.ViaCepService;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProvaApplicationTests {
 
 	@Autowired
 	ViaCepService viacepService;
 	
+	 @Autowired
+	 MockMvc mockMvc;
+	 
+	
 	@Test
-	void contextLoads() {
-		ViaCepResponse res = viacepService.getCep("99711082");
-		assertEquals(res.getBairro(), "Espírito Santo");
+	void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/cep/99711082")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString("Erechim")));
 	}
 
 }
